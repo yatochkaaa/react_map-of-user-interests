6518432b9d22974d7be08c5cd5f777be57eceee5
Yatochka <volodymyr.taranin@gmail.com>
2025-12-16 17:34:43 +0200

    users generate count to constant

--- scripts/generate-users.ts
+++ scripts/generate-users.ts
@@ -3,9 +3,10 @@ import { writeFileSync, mkdirSync } from "fs";
 import path from "path";
 import { interestsList } from "../src/constants.ts";
 
+const USERS_COUNT = 10000;
 const outputPath = path.join(process.cwd(), "public", "users.json");
 
-const users = Array.from({ length: 10000 }, () => ({
+const users = Array.from({ length: USERS_COUNT }, () => ({
   _id: faker.string.uuid(),
   lat: faker.location.latitude(),
   lon: faker.location.longitude(),
